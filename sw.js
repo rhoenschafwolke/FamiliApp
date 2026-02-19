176e11fe9aa0d6c8a72a68f3f38ecb6de37ee8b9
rhoenschafwolke <rhoenschafwolke@gmail.com>
2026-02-19 16:53:33 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'familiapp-v1';
+const URLS = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
